2cea8ca60fc329d2312929eeaab3d125b26d3092
上田良江 <uedayoshie@uedayoshienoMBP.lan>
2026-01-27 20:07:35 +0900

    Force Google account selection on login

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -16,3 +16,4 @@ const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
 export const googleProvider = new GoogleAuthProvider();
+googleProvider.setCustomParameters({ prompt: 'select_account' });
